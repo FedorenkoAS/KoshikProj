31fb34e93bda6dfebd7dab9ef6c331e8b66f99d7
test <fedorenko.aleks.serg@gmail.com>
2017-11-17 15:04:20 +0200

    FirstTest

--- src/test/java/City24/City24Pay.java
+++ src/test/java/City24/City24Pay.java
@@ -1,5 +1,6 @@
 package City24;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -15,19 +16,23 @@ public class City24Pay {
     @Test
     public void City24PayMobile() {
         File fileGC = new File("./drivers/chromedriver.exe");
-        System.setProperty("Webdriver.chrome.driver", fileGC.getAbsolutePath());
+        System.setProperty("webdriver.chrome.driver", fileGC.getAbsolutePath());
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         driver.manage().window().maximize();
 
         driver.get("https://koshik.net/");
         driver.manage().timeouts().implicitlyWait(2,TimeUnit.SECONDS);
-        driver.findElements(By.id("city24")).click;
+        driver.findElement(By.id("city24")).click();
         driver.manage().timeouts().implicitlyWait(2,TimeUnit.SECONDS);
-        driver.findElements(By.id("id=1")).click();
+        driver.findElement(By.id("id=1")).click();
         driver.manage().timeouts().implicitlyWait(2,TimeUnit.SECONDS);
-        driver.findElements(By.id("16217")).click();
+        driver.findElement(By.id("16217")).click();
         driver.manage().timeouts().implicitlyWait(2,TimeUnit.SECONDS);
-        driver.findElements(By.id("tel_number")).sendKeys("380969294968");
+        driver.findElement(By.id("tel_number")).sendKeys("380969294968");
+        driver.findElement(By.id("amount")).sendKeys("150");
+        driver.findElement(By.id("ok-btn")).click();
+        driver.findElement(By.id("generate-batch-button")).click();
+        Assert.assertTrue(driver.findElement(By.className("modal-body")).isDisplayed());
     }
 }
